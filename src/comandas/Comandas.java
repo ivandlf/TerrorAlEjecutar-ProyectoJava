12d6b0802ff03496b0383eced71dce2d237b4053
ivandlf <98287091+ivandlf@users.noreply.github.com>
2022-12-03 00:18:07 -0300

    comandas desde menu principal

--- src/comandas/Comandas.java
+++ src/comandas/Comandas.java
@@ -4,71 +4,10 @@ package comandas;
 import java.util.Scanner;
 
 public class Comandas {
-    public static void main(String[] args) {
+    public static void Comandas(double ganancias, double sbebida[], double valorm, double valorsnk, double valorp, double valorb, double shamburguesa, double squeso, double spanl, double spanh, double sharina, double smilanesa, double ordenessumatoria, String listacomanda[][], double ordenes, double preciop[], double preciom[], double preciob[], String pizza[], String bebida[], String minuta[], String bandera) {
         Scanner entrada = new Scanner(System.in);
        // Le damos valores a las distintas comidas,productos y stock
-        int ordenessumatoria = 0;
-	int valorB = 2;
-        int valorP = 2;
-	int valorSNK = 2;
-        int valorM = 2;
-	String bandera = "";
-        int ordenes = 0;
-	int gastos = 0;
-        int ganancias = 0;
-	int Shamburguesa = 0;
-        int Squeso = 0;
-	int SpanL = 0;
-        int SpanH = 0;
-	int Smilanesa = 0;
-        int Sharina = 0;
-	int bebidas = 0;
-        int Ssnack[] = new int [3];
-        Ssnack [0] = 0;
-	Ssnack[1] = 0;
-        Ssnack[2] = 0;
-        int precioP[]= new int [4];
-	precioP[0] = 700;
-        precioP[1] = 800;
-	precioP[2] = 900;
-        precioP[3]= 0;
-        int precioM[] = new int [4];
-        precioM[0] = 500;
-	precioM[1] = 800;
-        precioM[2] = 650;
-        precioM[3] = 0;
-        int precioB[] = new int [4];
-	precioB[0] = 290;
-        precioB[1] = 400;
-	precioB[2] = 900;
-        precioB[3] = 0;
-        int precioSNK[] = new int [3];
-        precioSNK[0] = 100;
-	precioSNK[1] = 150;
-        precioSNK[2] = 200;
-        String minuta [] = new String [4];
-	minuta[0] = "Milanesas";
-        minuta[1] = "Lomos";
-	minuta[2] = "Hamburguesa";
-        minuta[3] = "Nada";
-        String pizza[] = new String[4];
-        pizza[3] = "Nada";
-        pizza[0] = "Especial";
-	pizza[1] = "Fugaceta";
-        pizza[2] = "Napolitana";
-        String bebida[] = new String[4];
-	bebida[0] = "Coca Cola";
-        bebida[1] = "Cerveza";
-	bebida[2] = "Fernet";
-        bebida[3] = "Nada";
-        int Sbebida[]= new int[3];
-        Sbebida[0] = 0;
-	Sbebida[1] = 0;
-        Sbebida[2] = 0;
-        String snack[] = new String [3];
-	snack[0] = "Palitos Salados";
-        snack[1] = "Mani Cervecero";
-	snack[2] = "Papas Fritas"; 
+         
         int precioTotal = 0;
         
       
@@ -96,7 +35,7 @@ public class Comandas {
                         System.out.println("elija su opcion: ");
                         int opcionPi = entrada.nextInt();
                         listaComandas[i][2]= pizza[opcionPi];
-                        precioTotal += precioP[opcionPi];
+                        precioTotal += preciop[opcionPi];
                     }
                     case 3 -> {
                         System.out.println("Minutas: ");
@@ -106,7 +45,7 @@ public class Comandas {
                         System.out.println("elija su opcion: ");
                         int opcionMi = entrada.nextInt();
                         listaComandas[i][3]= minuta[opcionMi];
-                        precioTotal += precioP[opcionMi];
+                        precioTotal += preciop[opcionMi];
                     }
                     case 4 -> {
                         System.out.println("Bebidas: ");
@@ -116,7 +55,7 @@ public class Comandas {
                         System.out.println("elija su opcion: ");
                         int opcionBe = entrada.nextInt();
                         listaComandas[i][4] = bebida[opcionBe];
-                        precioTotal += precioP[opcionBe];
+                        precioTotal += preciop[opcionBe];
                     }
                     default -> {
                             System.out.println("ingrese "+listaComandas[0][j]);
