1cc5704277578b4b3935ca069bb59ba588b76b66
AquilesMM <112222511+AquilesMM@users.noreply.github.com>
2022-12-08 11:36:00 -0300

    arreglo en MenuSnack

--- src/controlstock/MenuSnack.java
+++ src/controlstock/MenuSnack.java
@@ -6,7 +6,7 @@ package controlstock;
 
 public class MenuSnack {
 
-    public static void menuSnack(double[] Ssnack, double valorSNK, double[] Snack) {
+    public static void menuSnack(double[] Ssnack, double valorSNK, String[] Snack) {
         int i;
         // Mostramos todo el menú de snacks y la cantidad de stock disponible.
         System.out.println("---------------------------------------");        
@@ -16,4 +16,6 @@ public class MenuSnack {
         }
         
     }
+
+   
 }
