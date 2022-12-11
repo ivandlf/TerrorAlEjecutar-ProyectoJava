89a4c95c4f65ba40f15684a65bba031db82c01ec
AquilesMM <112222511+AquilesMM@users.noreply.github.com>
2022-12-10 23:10:59 -0300

    version megafinal

--- src/controlstock/EntradaBebidas.java
+++ src/controlstock/EntradaBebidas.java
@@ -9,33 +9,34 @@ public class EntradaBebidas {
         int i, opcion, aux;
         double precio;
         Scanner teclado = new Scanner(System.in);
-        System.out.println("");
-        System.out.println("------------------------------------");
-        System.out.println("***** INGRESAR STOCK (Bebidas) *****");
-        System.out.println("------------------------------------");
-        for (i = 0;  i <= valorB[0]; i++) {
-            System.out.println("" + i + "_ " + bebida[i]);
-        }
-        System.out.println(Math. round(valorB[0] + 1) + "_volver");
-        System.out.println("------------------------------------");
-        opcion = Integer.parseInt(teclado.next()); // Leemos la opcion de bebida
         do{
-             if (opcion <= valorB[0]) {
-            System.out.println("Ingrese la cantidad de botellas ingresadas: ");
-            aux = Integer.parseInt(teclado.next());
-            Sbebida[opcion] = Sbebida[opcion] + aux; // Aumentamos la cantidad ingresada al stock total
             System.out.println("");
-            System.out.println("Ingrese precio por botella");
-            precio = Double.parseDouble(teclado.next());
-            gastos[0] += (precio + aux); // Sumamos los gastos de la compra de las bebudas a gastis totales
-        }
-        if(opcion > (valorB[0]+1)){
-            System.out.println("------------------------------------------------------------------------------------------------");
-                                System.out.println("| La opción que ha ingresado no es correcta. Por favor, digite una que se encuentre en el menú. |");
-                                System.out.println("------------------------------------------------------------------------------------------------");
-                                System.out.println("");
-                                System.out.println("");
-        }
+            System.out.println("------------------------------------");
+            System.out.println("***** INGRESAR STOCK (Bebidas) *****");
+            System.out.println("------------------------------------");
+            for (i = 0;  i <= valorB[0]; i++) {
+                System.out.println("" + i + "_ " + bebida[i]);
+            }
+            System.out.println(Math. round(valorB[0] + 1) + "_volver");
+            System.out.println("------------------------------------");
+            opcion = Integer.parseInt(teclado.next()); // Leemos la opcion de bebida
+            
+                if (opcion <= valorB[0]) {
+                System.out.println("Ingrese la cantidad de botellas ingresadas: ");
+                aux = Integer.parseInt(teclado.next());
+                Sbebida[opcion] = Sbebida[opcion] + aux; // Aumentamos la cantidad ingresada al stock total
+                System.out.println("");
+                System.out.println("Ingrese precio por botella");
+                precio = Double.parseDouble(teclado.next());
+                gastos[0] += (precio + aux); // Sumamos los gastos de la compra de las bebudas a gastis totales
+            }
+            if(opcion > (valorB[0]+1)){
+                System.out.println("------------------------------------------------------------------------------------------------");
+                                    System.out.println("| La opción que ha ingresado no es correcta. Por favor, digite una que se encuentre en el menú. |");
+                                    System.out.println("------------------------------------------------------------------------------------------------");
+                                    System.out.println("");
+                                    System.out.println("");
+            }
 
         }while(opcion != (valorB[0]+1)
        
